44ba68ee0a03c3a5dfbdb2b8c22f8dd6cd08b15e
ianT123456 <62101435+ianT123456@users.noreply.github.com>
2021-05-31 08:57:23 -0400

    Add files via upload

---
+++ eje1/Client.java
@@ -0,0 +1,17 @@
+package eje1;
+
+public class Client {
+    public static void main(String [] args){
+        Colegio utasawa = new Colegio();
+
+        utasawa.Matricularse(new Estudiante("Martin","0000"),100 );
+
+        utasawa.Matricularse(new Estudiante("nicolas","6666"),200 );
+
+        utasawa.Matricularse(new Estudiante("Tilin","999"),300 );
+
+        utasawa.Matricularse(new Estudiante("anuel","5555"),400 );
+
+        utasawa.consultarListaEstudiantes(new Joven("anuel","5555","nnnn"));
+    }
+}
---
+++ eje1/Ventanilla.java
@@ -0,0 +1,59 @@
+package eje1;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ventanilla {
+    private static Ventanilla instance = null;
+
+    private Map<String, Estudiante> Estudianmap = new HashMap<>();
+    private Map<String, String> estudianMap = new HashMap<>();
+    private Map<String, Integer> pagoEstudiante = new HashMap<>();
+    private int amount = 0;
+
+    private Ventanilla() {
+    }
+
+    private synchronized static void makeInstance() {
+        if (instance == null)
+            instance = new Ventanilla();
+    }
+
+    public static Ventanilla getInstance() {
+        if (instance == null)
+            makeInstance();
+
+        return instance;
+    }
+
+    public void anhadirEstudiante(Estudiante estudiante, int pagoMatricula) {
+        if (!Estudianmap.containsKey(estudiante.getId())) {
+            Estudianmap.put(estudiante.getId(), estudiante);
+            estudianMap.put(estudiante.getId(), new Date().toString());
+            pagoEstudiante.put(estudiante.getId(), pagoMatricula);
+            amount = amount + pagoMatricula;
+            System.out.println("Matriculando ...");
+            System.out.println("Datos " + estudiante.getId() + "\n" + estudiante.getNombre() + "\n" + pagoMatricula);
+        } else {
+            System.out.println("Posee Matricula");
+            System.out.println("Declined" + estudiante.getId() + "\n" + estudiante.getNombre() + "\n" + pagoMatricula);
+
+        }
+
+    }
+
+    public void consultaDatos() {
+        for (String key : Estudianmap.keySet()
+        ) {
+            System.out.println(" CI:" + Estudianmap.get(key).getId());
+            System.out.println(" Nombre:" + Estudianmap.get(key).getId());
+            System.out.println(" Matricula " + pagoEstudiante.get(key));
+            System.out.println(" Tiempo " + estudianMap.get(key));
+        }
+
+        System.out.println("  Total Recaudado " + amount);
+    }
+
+
+}
